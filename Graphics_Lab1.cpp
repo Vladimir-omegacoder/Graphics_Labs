fddf9b3ef5dc6ba7c24e3684a7fe0ce032bbfc58
Vladimir <supervova0505@gmail.com>
2023-09-26 11:13:57 +0300

    First commit

---
+++ Graphics_Lab1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include "SFML/Graphics.hpp"
+
+
+
+
+int main()
+{
+
+    sf::RenderWindow main_window(sf::VideoMode(800, 600), "Graphics window");
+
+	while (main_window.isOpen())
+	{
+
+		sf::Event main_event;
+
+		while (main_window.pollEvent(main_event))
+		{
+
+			if (main_event.type == sf::Event::Closed)
+			{
+				main_window.close();
+			}
+
+		}
+
+		main_window.display();
+
+	}
+
+	
+
+}
+
+
